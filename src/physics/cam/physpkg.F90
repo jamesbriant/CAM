eba7ed19c76f43b73b2c1a75c9da9eef178eefa6
Scott <none@gmail.com>
2025-01-08 18:47:44 -0500

    Added NN, but I can't test

--- src/physics/cam/physpkg.F90
+++ src/physics/cam/physpkg.F90
@@ -35,6 +35,9 @@ module physpkg
   use modal_aero_calcsize,    only: modal_aero_calcsize_init, modal_aero_calcsize_diag, modal_aero_calcsize_reg
   use modal_aero_wateruptake, only: modal_aero_wateruptake_init, modal_aero_wateruptake_dr, modal_aero_wateruptake_reg
 
+   ! Custom 
+   use cam_nn,             only: torch_inference
+
   implicit none
   private
   save
@@ -1147,6 +1150,9 @@ contains
     call t_barrierf('sync_ac_physics', mpicom)
     call t_startf ('ac_physics')
     call t_adj_detailf(+1)
+
+    ! Add ML model
+    call torch_inference(phys_state)
 
 !$OMP PARALLEL DO PRIVATE (C, NCOL, phys_buffer_chunk)
 
